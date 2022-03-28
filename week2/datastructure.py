76b2cd3ff2621b39192fbf287ad1c91a5e87735b
jas528 <76809910+jas528@users.noreply.github.com>
2022-03-28 13:35:24 -0500

    week2

---
+++ week2/datastructure.py
@@ -0,0 +1,22 @@
+#tuple 
+Data1 = (7, False, "Apple", True, 7,98.61)
+print(len(Data1))
+
+#set
+Data2 = {"July", 4, 8,"Tango",4.3, "Bingo"}
+print (Data2)
+#list[]
+Data3= ["A", 7,-1, 3.14, True, 7] 
+Data3.reverse()
+Data3[1] ="B"
+Data3.pop()
+print (Data3)
+Data4 ={
+    "name": "Joe",
+     "age": 26,
+    "active": True,
+    "hourly_wage" : 14.75} #dictionary
+Data4["active"]=False
+Data4["adress"] ="123 West Main Street"
+print(Data4["hourly_wage"]*40)
+print(Data4)
