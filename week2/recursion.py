d574bb4452f4f5549401b3a7ecd1fc951fb66ca7
jas528 <76809910+jas528@users.noreply.github.com>
2022-03-29 16:57:16 -0500

    week2

---
+++ week2/recursion.py
@@ -0,0 +1,37 @@
+def loop1():
+    # Sum the odd numbers between 1 and 20
+    odd_sum = 0
+    for i in range(20):
+        if (i % 2) == 1:
+            odd_sum += i
+    return odd_sum
+
+def loop2():
+    # Sum the even numbers between 1 and 20
+    i = 0
+    even_sum = 0
+    while i < 20:
+        if (i % 2) == 0:
+            even_sum += i
+        i += 1
+    return even_sum
+
+def loop1Rec(num,odd_sum):
+    # Duplicate the loop1 function using recursion
+    if (num >= 20):
+        return odd_sum
+    elif (num % 2) == 1:
+        odd_sum += num
+        # skip even numbers to reduce
+    return loop2Rec (num+2,odd_sum)
+
+    
+def loop2Rec(num,even_sum):
+    # Duplicate the loop2 function using recursion
+    if (num >= 20):
+        return even_sum
+    elif (num % 2) == 0:
+        even_sum += num
+        # skip odd numbers to reduce
+    return loop2Rec (num+2,even_sum)
+
